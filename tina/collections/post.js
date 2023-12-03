679a1a471d314e5a6546872a1b8097843f7f1fe7
Fran <fm-a@robindatahood.com>
2023-12-03 18:54:32 +0100

    mdx modified

--- tina/collections/post.js
+++ tina/collections/post.js
@@ -2,28 +2,71 @@
  * @type {import('tinacms').Collection}
  */
 export default {
-  label: "Blog Posts",
+  label: "Posts",
   name: "post",
-  path: "content/post",
+  path: "content/posts",
+  format: "mdx",
   fields: [
     {
-      type: "string",
-      label: "Title",
-      name: "title",
+      type: 'string',
+      label: 'Title',
+      name: 'title',
+      required: true
     },
     {
-      type: "string",
-      label: "Blog Post Body",
+      type: 'datetime',
+      label: 'Date',
+      name: 'date',
+      required: true
+    },
+    {
+      label: 'Tags',
+      name: 'tags',
+      type: 'string',
+      list: true,
+      required: false
+    },
+    {
+      type: 'string',
+      label: 'Category',
+      name: 'category',
+      required: true
+    },
+    {
+      type: 'string',
+      label: 'Summary',
+      name: 'summary',
+      required: false
+    },
+    {
+      type: 'string',
+      label: 'Book Author',
+      name: 'bookAuthor',
+      required: false
+    },
+    {
+      type: 'string',
+      label: 'Book Year',
+      name: 'bookYear',
+      required: false
+    },
+    {
+      type: 'string',
+      label: 'Cover Image',
+      name: 'coverImage',
+      required: true
+    },
+    {
       name: "body",
+      label: "Main Content",
+      type: "rich-text",
       isBody: true,
-      ui: {
-        component: "textarea",
-      },
     },
   ],
   ui: {
     router: ({ document }) => {
-      return `/posts/${document._sys.filename}`;
+      // return `/posts/${document._sys.filename}`;
+      return undefined
     },
   },
 };
